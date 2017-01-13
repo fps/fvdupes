ce86d4c2944b62e32083970a4a033a0f2aa7decb
Florian Paul Schmidt <mista.tapas@gmx.net>
2017-01-13 13:16:14 +0100

    output some stuff

--- fvdupes.sh
+++ fvdupes.sh
@@ -1,9 +1,19 @@
 
 STORES=$*
 
+echo '# generating thumbs...'
 ./generate_thumbs.sh $STORES
+
+echo '# collecting thumbs...'
 ./collect_thumbs.sh $STORES
+
+echo '# hashing thumbs...'
 ./hash_thumbs.sh $STORES
+
+echo '# matching hashes...'
 ./match_hashes.sh
+
+echo '# checking for hash dupes...'
 ./check_hash_dupes.sh
 
+echo '# done. exiting.'
--- generate_thumbs.sh
+++ generate_thumbs.sh
@@ -1,1 +1,3 @@
+frames="0000005000 0000015000"
+
 find $* -type f -exec openthumbnailer -i {} -o {}_thumbnail_%010d.jpg  -f 5000 -f 15000 \;
